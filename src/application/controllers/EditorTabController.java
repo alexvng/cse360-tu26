ef43d93a88ca269f75fb664c1d4ec2e20f042ff6
ccoppo <148139135+ccoppo@users.noreply.github.com>
2023-11-30 23:33:20 -0700

    Update EditorTabController.java
    changes to editor Tab controller

--- src/application/controllers/EditorTabController.java
+++ src/application/controllers/EditorTabController.java
@@ -3,8 +3,33 @@ package application.controllers;
 
 public class EditorTabController {
 	
-	public EditorTabController() {
-		// TODO Auto-generated constructor stub
+@FXML private ChoiceBox <String> projectsBox; 
+	@FXML private ChoiceBox <String> EffortLogBox; 
+	@FXML private ChoiceBox <String> LifeCycleStepsBox; 
+	@FXML private ChoiceBox <String> EffortCategoryBox; 
+	@FXML private ChoiceBox <String> PlanBox; 
+	
+	
+	List<String> names = new ArrayList<String>();
+	List<String> logs = new ArrayList<String>();
+	
+	public void initialize(URL url, ReourceBundle rb) { 
+		
+		for(EffortLog log: GlobalObjects.effortLogList.getEffortLogList()) {
+			logs.add(log.getName());
+			System.out.println(log.getName());
+		}
+		
+		for(Project proj: GlobalObjects.projList.getProjList()) {
+			names.add(proj.getName());
+		}
+		
+		
+		projectsBox.getItems().addAll(names);
+		effortLogBox.getItems().addAll(logs);
+		lifeCycleStepsBox.getItems().addAll(GlobalObjects.cycleSteps);
+		effortCategoryBox.getItems().addAll(GlobalObjects.effortCategory);
+		
 	}
 
 }
